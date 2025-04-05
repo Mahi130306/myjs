09487645a3837189f276770c8c6041388b05b974
Mahi H H <137616109+mist2458@users.noreply.github.com>
2025-04-05 14:02:00 +0530

    html created 3

---
+++ js/vanilla/lecture3.js
@@ -0,0 +1,11 @@
+// console.log("Hi, Earth!");
+// loops and strings
+console.log("loops and strings");
+// loops
+// for loop, while loop, do while loop, for of loop, for in loop
+
+// for loop
+console.log("for loop");
+for (let i = 0; i <= 115151; i++) {
+    console.log("kkk");
+}
