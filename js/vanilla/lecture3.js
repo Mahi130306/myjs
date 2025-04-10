4f3bb66c6c4bd763118936c81d072c2622421793
Mahi H H <137616109+mist2458@users.noreply.github.com>
2025-04-10 11:05:59 +0530

    string methods

--- js/vanilla/lecture3.js
+++ js/vanilla/lecture3.js
@@ -65,7 +65,7 @@ for (let num=0; num<=10; num++){
 }
 // 2. create a game where you start with any random game number.Ask the user to keep guessing the game number until the user enters correct value.
 let gnum = 25;
-let unum = (prompt("Guess the number"));
+let unum = (prompt("Guess the number 0 to 100"));
 
 while (unum != gnum) {
     if (unum < gnum) {
@@ -94,5 +94,18 @@ console.log(typeof sentence);
  console.log("the price of ",obj2.item," is ",obj2.price); // $ is a placeholder for the value of obj2.item and obj2.price
  let output = `the price of ${obj2.item} is ${obj2.price}`;
  console.log(output);
+// new line character
+let str2 = "Hi\nEarth!"; // \n is a new line character
+console.log(str2);
+// tab character
+let str3 = "Hi\t Earth!"; // \t is a tab character
+console.log(str3);
 
- 
+// string methods
+console.log("string methods");
+let str4 = "Hi, earth!";
+console.log(str4); // prints the string
+console.log("length of above string is",str4.length); // length of the string
+console.log("string in uppercase",str4.toUpperCase()); // converts the string to uppercase
+console.log("string in lowercase",str4.toLowerCase()); // converts the string to lowercase  
+console.log("string in titlecase",str4.charAt(0).toUpperCase() + str4.slice(1)); // converts the first character of the string to uppercase and the rest to lowercase
