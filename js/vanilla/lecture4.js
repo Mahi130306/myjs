0ea73abcf1298f7356a07d34568a26866c932bd9
Mahi H H <137616109+mist2458@users.noreply.github.com>
2025-04-22 23:28:21 +0530

    array method start

--- js/vanilla/lecture4.js
+++ js/vanilla/lecture4.js
@@ -23,3 +23,37 @@ for (let i = 0; i < heroes.length; i++) {
 for (let i  of heroes) {
     console.log(i);
 }
+let cities = ["Delhi", "Mumbai", "Bangalore", "Chennai"]; // array of strings
+for (let city of cities){
+    console.log(city);
+    console.log(city.toUpperCase()); // convert to uppercase
+}
+
+
+//practice question 
+console.log("Practice Questions");
+// 1.fro a given arry with marks of student->[85,97,44,37,76,60] find the average marks of the entire class
+let marks1 = [85, 97, 44, 37, 76, 60]; // array of numbers
+let sum = 0; // variable to store the sum of marks
+for (let i=0; i<marks1.length; i++){
+    sum += marks1[i]; 
+}
+let cavg= sum/marks1.length; 
+console.log(`avg marks of class = ${cavg}`); 
+
+// 2.fro a given array with prices of 5 items -> [250,645,300,900,50] all items 
+// have an offer of 10% OFF on them. Change the array to store final price after 
+// applying offer
+let prices = [250, 645, 300, 900, 50]; 
+for (let i=0; i<prices.length; i++){
+    offer=prices[i]*0.1; 
+    prices[i] -= offer; 
+}
+console.log(`Final prices after offer = ${prices}`);
+
+// array methods
+// push(), pop(), shift(), unshift(), splice(), slice(), concat(), join(), indexOf(), lastIndexOf(), reverse(), sort()
+// push() - add an element to the end of the array
+console.log("Array Methods");
+let arr1 = [1, 2, 3, 4, 5]; // array of numbers
+console.log(arr1); // [1, 2, 3, 4, 5]
