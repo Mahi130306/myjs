7e9485d933cc45e5dbb02e2eb499117c385988d3
Mahi H H <137616109+mist2458@users.noreply.github.com>
2025-04-25 23:53:58 +0530

    l4 array methods  updated

--- js/vanilla/lecture4.js
+++ js/vanilla/lecture4.js
@@ -53,7 +53,43 @@ console.log(`Final prices after offer = ${prices}`);
 
 // array methods
 // push(), pop(), shift(), unshift(), splice(), slice(), concat(), join(), indexOf(), lastIndexOf(), reverse(), sort()
+
 // push() - add an element to the end of the array
 console.log("Array Methods");
 let arr1 = [1, 2, 3, 4, 5]; // array of numbers
 console.log(arr1); // [1, 2, 3, 4, 5]
+arr1.push(6); // add 6 to the end of the array
+console.log(arr1); // [1, 2, 3, 4, 5, 6]
+arr1.push(7, 8, 9, "apple"); // add 7, 8, 9, apple to the end of the array
+console.log(arr1); // [1, 2, 3, 4, 5, 6, 7, 8, 9, "apple"]
+
+// pop() - removes an element from the arry 
+let del = arr1.pop(); // remove the last element from the array and store it in 'del'
+console.log(arr1); // log the updated array to the console
+console.log("deleted element = " + del); // log the deleted element to the console
+
+// concat() - join two arrays
+let arr2 = [10, 20, 30]; // array of numbers
+console.log("arr2",arr2); // [10, 20, 30]
+let arr3 = arr1.concat(arr2); // join arr1 and arr2
+console.log("arr3",arr3); // [1, 2, 3, 4, 5, 6, 7, 8, 9, "apple", 10, 20, 30]
+
+// unshift() - add an element to the beginning of the array
+let arr4 = [1, 2, 3, 4, 5]; // array of numbers
+console.log(arr4); // [1, 2, 3, 4, 5]
+arr4.unshift(0); // add 0 to the beginning of the array
+console.log(arr4); // [0, 1, 2, 3, 4, 5]
+arr4.unshift(-1, -2, -3); // add -1, -2, -3 to the beginning of the array
+console.log(arr4); // [-1, -2, -3, 0, 1, 2, 3, 4, 5]
+
+// shift() - remove the first element from the array
+let arr5 = [1, 2, 3, 4, 5]; // array of numbers
+console.log(arr5); // [1, 2, 3, 4, 5]
+let del1 = arr5.shift(); // remove the first element from the array and store it in 'del1'
+
+// slice() - extract a part of the array
+let arr6 = [1, 2, 3, 4, 5]; // array of numbers
+console.log(arr6); // [1, 2, 3, 4, 5]
+let arr7 = arr6.slice(1, 4); // extract elements from index 1 to 4 (not including 4)
+console.log(arr7); // [2, 3, 4]
+
